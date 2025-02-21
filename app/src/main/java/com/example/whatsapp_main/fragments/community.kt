a9a91046ba7a590b859336f0e08b2c4e593c78e8
Atul Kumar Singh <atul.k.singh5002@gmail.com>
2025-02-21 22:15:42 +0530

    Chats Adapter fixed by fixing binding in chats fragment but status fragment is crashing

---
+++ app/src/main/java/com/example/whatsapp_main/fragments/community.kt
@@ -0,0 +1,7 @@
+package com.example.whatsapp_main.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.whatsapp_main.R
+
+class community : Fragment(R.layout.fragment_community) {
+}
